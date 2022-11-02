714732cf284297afc3982a16dae6fcbf0f4a1f0b
Nicolas Becharat <78233186+justniicolas@users.noreply.github.com>
2022-11-02 20:01:23 +0100

    Add files via upload

--- Experience.js
+++ Experience.js
@@ -35,7 +35,7 @@ export default function Experience()
                     width={ 2.5 }
                     height={ 1.65 }
                     intensity={ 65 }
-                    color={ '#ff6900' }
+                    color={ '#1A37A0' }
                     rotation={ [ - 0.1, Math.PI, 0 ] }
                     position={ [ 0, 0.55, - 1.15 ] }
                 />
